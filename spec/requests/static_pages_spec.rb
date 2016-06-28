1dcafeada99b00f58df241e0d483d744253b220e
angus <85288082@qq.com>
2016-06-28 23:51:33 +0800

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,36 +10,41 @@ require 'rails_helper'
 #end
 
 describe "Static pages" do
-  describe "Home page" do
-    it "should have the contend 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+	describe "Home page" do
+    		it "should have the contend 'Sample App'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Sample App')
+		end
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App ")
+		end
+		it "should not have custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
+		end
 	end
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-	end
-  end
 
-  describe "Help Page" do 
-	it "should have the content 'help'" do
-	  visit '/static_pages/help'
-	  expect(page).to have_content('Help')
-    end
-	it "should have the right title" do
-	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	describe "Help Page" do 
+		it "should have the content 'help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
-  end
 
-  describe "About page" do
-	it "should have the content 'About Us'" do
-	  visit '/static_pages/about'
-	  expect(page).to have_content('About')
+	describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About')
+		end
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		end
 	end
- 	it "should have the right title" do
-	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-	end end
 end
 
